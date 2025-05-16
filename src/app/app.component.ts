be183392ab37f4acab81ad2a18ab692dabaad7fd
Josué Peñaloza <josue20650@gmail.com>
2025-05-16 13:28:16 -0400

    Diseño responsivo bastante decente, formulario realizado con validaciones basicas por el momento

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,28 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class AppComponent {
   title = 'LarryTech';
-  numeroOrden = '';
-  fechaRecepcion = '';
+  formulario = new FormGroup({
+    numeroOrden: new FormControl('', Validators.required),
+    fechaRecepcion: new FormControl('', Validators.required),
+    cliente: new FormControl('', [Validators.required, Validators.pattern(/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/)]),
+    rut: new FormControl('', Validators.required),
+    telefono: new FormControl('', Validators.required),
+    equipo: new FormControl('', Validators.required),
+    marca: new FormControl('', Validators.required),
+    modelo: new FormControl('', Validators.required),
+    numeroSerie: new FormControl('', Validators.required),
+    desperfeccion: new FormControl('', Validators.required),
+    diagnostico: new FormControl('', Validators.required),
+    modoPago: new FormControl('', Validators.required),
+    abono: new FormControl('', Validators.required),
+    saldo: new FormControl('', Validators.required),
+    valorTotal: new FormControl('', Validators.required),
+    fechaEntrega: new FormControl('', Validators.required),
+  });
+
+
+  onSubmit() {
+    console.warn(this.formulario.value)
+  }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { FormularioComponent } from './formulario/formulario.component';
+import { ListaComponent } from './lista/lista.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioComponent,
+    ListaComponent
   ],
   imports: [
     BrowserModule,
